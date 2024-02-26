c0161efcfbb8dbf81adc252eddd701d581db1036
musabinshabeer <musabinshabeer@gmail.com>
2024-02-26 15:15:19 +0500

    User Auth set

--- Extensions/Middleware/UserAuthorizeAttribute.cs
+++ Extensions/Middleware/UserAuthorizeAttribute.cs
@@ -8,16 +8,12 @@ namespace BISPAPIORA.Extensions.Middleware
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class UserAuthorizeAttribute : Attribute, IAuthorizationFilter
     {
-        private readonly IConfiguration configuration;
-        private string ApiKeyHeaderName = "";
-        public UserAuthorizeAttribute( IConfiguration configuration)
+        public UserAuthorizeAttribute( )
         {
-            this.configuration = configuration;
-            ApiKeyHeaderName = configuration.GetSection("Api-Key:Header_key").Value ?? "";
+           
         }
         public void OnAuthorization(AuthorizationFilterContext context)
         {
-            string apiKey = context.HttpContext.Request.Headers[ApiKeyHeaderName];
             var allowAnonymous = context.ActionDescriptor.EndpointMetadata.OfType<AllowAnonymousAttribute>().Any();
             if (allowAnonymous)
                 return;
