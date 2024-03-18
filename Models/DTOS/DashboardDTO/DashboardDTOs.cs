7d9fe189c4330f31aca24516415485681a6bb820
musabinshabeer <musabinshabeer@gmail.com>
2024-03-18 17:37:46 +0500

    dASHBOARD sTATS

--- Models/DTOS/DashboardDTO/DashboardDTOs.cs
+++ Models/DTOS/DashboardDTO/DashboardDTOs.cs
@@ -5,4 +5,13 @@
         public int registeredCount { get; set; } = 0;
         public int enrolledCount { get; set; } = 0;
     }
+    public class DashboardCitizenBaseModel
+    {
+        public Guid citizen_id { get; set; }
+        public string citizen_name { get; set; } =string.Empty;
+        public Guid registered_by { get; set; }= default!;
+        public DateTime registered_date{ get; set; }= default!;
+        public Guid enrolled_by { get;set; }= default!;
+        public DateTime enrolled_date { get;set; }= default!;
+    }
 }
